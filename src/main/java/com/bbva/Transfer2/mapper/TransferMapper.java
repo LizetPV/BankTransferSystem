a82f787cc9a2b2e7ad5e302e2ca3c8c8815f458b
Yesenia <lizet.peche@gmail.com>
2023-11-29 16:03:56 -0500

    Proyecto corriendo con solicitudes GET y POST

--- src/main/java/com/bbva/Transfer2/mapper/TransferMapper.java
+++ src/main/java/com/bbva/Transfer2/mapper/TransferMapper.java
@@ -3,10 +3,6 @@ package com.bbva.Transfer2.mapper;
 import com.bbva.Transfer2.DTOs.TransferDTO;
 import com.bbva.Transfer2.model.Transfer;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-
-import java.util.List;
-
 
 @Mapper(componentModel = "spring")
 public interface TransferMapper {
