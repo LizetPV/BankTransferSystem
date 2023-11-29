7ae70b1fce7edfa3d66841898e5af86043014293
Yesenia <lizet.peche@gmail.com>
2023-11-29 12:07:52 -0500

    Agregando test

---
+++ src/test/java/com/bbva/Transfer2/TransferTest.java
@@ -0,0 +1,15 @@
+package com.bbva.Transfer2;
+
+import com.bbva.Transfer2.DTOs.TransferDTO;
+import com.bbva.Transfer2.service.impl.TransferServiceImpl;
+import org.junit.jupiter.api.Test;
+
+
+public class TransferTest {
+/*    @Test
+    public void test1(){
+        TransferServiceImpl transferServiceImpl;
+        transferServiceImpl.getAllTransactions();
+        TransferDTO transferDTO = transferServiceImpl()
+    }*/
+}
