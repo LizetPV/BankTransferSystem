dc5a1ed4cf3af84369b48db5ab5889a2634137e6
Yesenia <lizet.peche@gmail.com>
2023-11-30 15:52:53 -0500

    corriendo test + apli completa hasta el punto 5

--- src/main/java/com/bbva/Transfer2/mapper/TransferMapper.java
+++ src/main/java/com/bbva/Transfer2/mapper/TransferMapper.java
@@ -3,10 +3,13 @@ package com.bbva.Transfer2.mapper;
 import com.bbva.Transfer2.DTOs.TransferDTO;
 import com.bbva.Transfer2.model.Transfer;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface TransferMapper {
+    @Mapping(target = "transferID", source = "transfer.transferID")
     TransferDTO ToTransferDTO(Transfer transfer);
-     Transfer toTransferEntity(TransferDTO transferDTO);
+    @Mapping(target = "transferID", ignore = true) // Ignore transferID during mapping from DTO to entity
+    Transfer toTransferEntity(TransferDTO transferDTO);
 
 }
